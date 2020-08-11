5579a8db973a7bbb4bae4e3c7c5742c2e7fe0160
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2020-08-11 11:27:22 -0300

    api_rest: a busca do dataset agora é feita utilizando um core(do solr) diferente

--- api_rest/app/thematic.py
+++ api_rest/app/thematic.py
@@ -1,11 +1,13 @@
 import pysolr
 
-solr = pysolr.Solr("http://localhost:8983/solr/main", always_commit=True)  # auth=<type of authentication>
-solr.ping()
+solr_resource = pysolr.Solr("http://localhost:8983/solr/resource", always_commit=True)  # auth=<type of authentication>
+solr_dataset = pysolr.Solr("http://localhost:8983/solr/dataset", always_commit=True)  # auth=<type of authentication>
+solr_resource.ping()
+solr_dataset.ping()
 
 
 def get_resource(query: str):
-    docs = solr.search(query, fl='id,score', rows=1000000000)
+    docs = solr_resource.search(query, fl='id,score', rows=1000000000)
     ids = {}
     for doc in docs:
         doc['score'] /= 100
@@ -14,10 +16,19 @@ def get_resource(query: str):
 
 
 def get_dataset(query: str):
-    docs = solr.search(query, **{'fl': 'num_resources_package', 'omitHeader': 'true', 'group': 'true',
-                       'group.field': 'package_id', 'group.limit': 1, 'rows': 1000000000})
+    docs = solr_dataset.search(query, fl='id,score', rows=1000000000)
     ids = {}
-    for doc in docs.grouped['package_id']['groups']:
-        doc_list = doc['doclist']
-        ids[doc['groupValue']] = doc_list['numFound']/doc_list['docs'][0]['num_resources_package']
+    for doc in docs:
+        doc['score'] /= 100
+        ids[doc['id']] = doc['score']
     return ids
+
+
+# def get_dataset(query: str):
+#     docs = solr.search(query, **{'fl': 'num_resources_package', 'omitHeader': 'true', 'group': 'true',
+#                        'group.field': 'package_id', 'group.limit': 1, 'rows': 1000000000})
+#     ids = {}
+#     for doc in docs.grouped['package_id']['groups']:
+#         doc_list = doc['doclist']
+#         ids[doc['groupValue']] = doc_list['numFound']/doc_list['docs'][0]['num_resources_package']
+#     return ids
