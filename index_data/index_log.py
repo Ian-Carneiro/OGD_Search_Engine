4c045cb0e5af1d3d0ca2d4f6ff3ffa92f8b400d2
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2020-09-27 13:15:33 -0300

    Correção do gitignore(removia csv com lugares) e adicionando verificação se pasta de logs está criada em index_data

--- index_data/index_log.py
+++ index_data/index_log.py
@@ -1,6 +1,11 @@
 import logging
 from logging.handlers import RotatingFileHandler
 from config import config
+import os
+
+if not os.path.exists("./logs/data_processor.log"):
+    os.mkdir("./logs")
+    os.mknod("./logs/data_processor.log")
 
 handler = RotatingFileHandler(config.log["file_name"], maxBytes=config.log["max_file_bytes"],
                               backupCount=config.log["backup_count"])
